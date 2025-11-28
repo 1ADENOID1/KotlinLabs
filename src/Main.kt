c9e60b92c13712170c0d8ed287f63eefdd204db1
1ADENOID1 <sashazaytzev2003@mail.ru>
2025-11-28 15:31:30 +0500

    Добавил примеры работы с Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,25 @@
 
 fun main() {
-    println("Hello world")
+    println("Hello world");
+
+    val inputNum : Int = 4;
+    var result : Int = 1;
+    for (i in 1..inputNum) {
+        result *= i;
+    }
+
+    println("The factorial of " + inputNum + " is: " + result);
+
+    val myList = mutableListOf(5, 1, 3, 0, 10);
+    println(myList);
+    for (i in 0..myList.size-2) {
+        for (j in 0..myList.size-i-2) {
+            if (myList[j] > myList[j+1]) {
+                val tmp = myList[j];
+                myList[j] = myList[j + 1]
+                myList[j + 1] = tmp;
+            }
+        }
+    }
+    println("Sorted list: " + myList);
 }
