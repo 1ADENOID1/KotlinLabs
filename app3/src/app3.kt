d26547f490b5dd4160d9e7db139cf11d9422b3d1
1ADENOID1 <sashazaytzev2003@mail.ru>
2025-12-03 18:13:58 +0500

    refactoring

--- app3/src/app3.kt
+++ app3/src/app3.kt
@@ -41,18 +41,41 @@ class Moto: Vehicle() {
 
 }
 
+open class Aquarium (open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
+    open var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+    open val shape = "rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
+    fun printSize() {
+        println(shape)
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+        // 1 l = 1000 cm^3
+        println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
+    }
+}
+
+class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+        // ellipse area = π * r1 * r2
+        get() = (width/2 * length/2 * height / 1000 * 3.14).toInt()
+        set(value) {
+            height = ((value * 1000 / 3.14) / (width/2 * length/2)).toInt()
+        }
+    override var water = volume * 0.8
+    override val shape = "cylinder"
+}
+
 
 fun main() {
-    val car = Car("Black")
-    println(car.getColor())
-
-    val moto = Moto()
-    moto.setSpeed(300)
-    println(moto.getSpeed())
-
-    println("list of classes")
-    val list = listOf(moto, car)
-    for (i in list) {
-        println(i.toString())
-    }
+    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
+    aquarium6.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
